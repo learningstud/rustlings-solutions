5c8436faeb5446ab48a4e37f76ea4349efeee879
learningstud <gordonhsu@potix.com>
2022-08-21 12:29:50 +0800

    variables3

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,9 +1,9 @@
 // variables3.rs
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::default;
 
 fn main() {
-    let x: i32;
+    let x: i32 = Default::default();
     println!("Number {}", x);
 }
